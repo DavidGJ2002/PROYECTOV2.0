d9290adade23a06b4307d8d875de5f4451457a1f
DavidGJ2002 <dgrandej2000@alumno.ipn.mx>
2022-11-08 11:50:00 -0600

    Update ventanapop.js

--- scripts/ventanapop.js
+++ scripts/ventanapop.js
@@ -3,8 +3,7 @@ window.addEventListener("load", function(){
         function open(event){
             document.querySelector(".anuncio").style.display = "block";
         },
-        1000
-    )
+        1000)
 });
 document.querySelector("#cerrar").addEventListener("click", function(){
     document.querySelector(".anuncio").style.display = "none";
